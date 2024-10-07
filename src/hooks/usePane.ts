b65546fdc7cfc23a75820597a9748dbb9289b128
RE <qq1609403959@outlook.com>
2024-10-07 16:54:47 +0800

    pane内置fps图表

--- src/hooks/usePane.ts
+++ src/hooks/usePane.ts
@@ -1,9 +1,20 @@
 import {Pane} from "tweakpane";
 import type {PaneConfig} from "tweakpane/dist/types/pane/pane-config";
 import {onUnmounted} from "vue";
+import {useFps} from "@vueuse/core";
 
 export function usePane(options: PaneConfig): Pane {
     const pane = new Pane(options);
+
+    const fps = useFps()
+    pane.addBinding(fps, 'value', {
+        label: 'FPS',
+        view: 'graph',
+        min: 0,
+        max: 300,
+        readonly: true,
+    })
+
     onUnmounted(() => {
         pane.dispose();
     });
