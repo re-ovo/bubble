9e00808243a25fdd55a6fadf918782fdc72a99a6
RE <qq1609403959@outlook.com>
2024-10-14 22:14:10 +0800

    test: 重写shader处理器单元用例

--- src/bubble/spec/wgsl.spec.ts
+++ src/bubble/spec/wgsl.spec.ts
@@ -1,21 +1,42 @@
 import {wgsl} from "@/bubble/shader/processor";
-import {expect, test} from "vitest";
+import {describe, expect, it} from "vitest";
 
-test("wgsl", () => {
-    let condition = false;
+describe("WGSL Shader Processor", () => {
+    it("test shader interpolation", () => {
+        const include = wgsl`
+        struct TestInterpolation {
+          @location(0) position: vec4<f32>
+        };
+        `
+        const shader = () => wgsl`
+            ${include}
+            
+            @vertex
+            fn main(input: TestInterpolation) -> TestInterpolation {
+                return input;
+            }
+        `;
 
-    const shader = () => wgsl`
-    // just test for if
-    #if ${condition}
-        let a = 1;
-    #else
-        let a = 2;   
-    #endif 
-    `
+        expect(shader())
+            .contains("struct TestInterpolation {")
+            .and.contains("fn main(input: TestInterpolation) -> TestInterpolation {");
+    })
+    it("test conditional marco", () => {
+        let condition = false;
 
-    condition = false;
-    expect(shader()).contains("let a = 2;").and.not.contains("let a = 1;");
+        const shader = () => wgsl`
+        // just test for if
+        #if ${condition}
+            let a = 1;
+        #else
+            let a = 2;   
+        #endif 
+        `
 
-    condition = true;
-    expect(shader()).contains("let a = 1;").and.not.contains("let a = 2;");
+        condition = false;
+        expect(shader()).contains("let a = 2;").and.not.contains("let a = 1;");
+
+        condition = true;
+        expect(shader()).contains("let a = 1;").and.not.contains("let a = 2;");
+    })
 })
