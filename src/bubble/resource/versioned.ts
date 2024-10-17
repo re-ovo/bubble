a0fc626f26b22a0f49a2ca522b74802d8b0a8351
RE <qq1609403959@outlook.com>
2024-10-17 23:31:29 +0800

    docs: version系统文档

--- src/bubble/resource/versioned.ts
+++ src/bubble/resource/versioned.ts
@@ -1,9 +1,25 @@
+/**
+ * 基于版本号的dirty标记系统
+ *
+ * 当这个对象的数据发生变化时，需要调用setNeedsUpdate方法，这个方法会将version加1
+ *
+ * 在判断是否需要更新时，通过比较it.version和cacheValue.version来判断
+ *
+ * 推荐配合VersionedCache使用
+ */
 export interface Versioned {
+    // 版本号，默认为0
     version: number;
 
     setNeedsUpdate(): void;
 }
 
+/**
+ * 基于WeakMap的带版本号的缓存系统
+ *
+ * @template K 缓存的key类型，必须实现Versioned接口
+ * @template V 缓存的value类型
+ */
 export class VersionedCache<K extends Versioned, V> {
     private cache: WeakMap<K, VersionedCacheValue<V>> = new Map();
 
