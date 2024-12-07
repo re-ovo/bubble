9230f528c635342043f8a71d875390c3d9ec4695
RE <qq1609403959@outlook.com>
2024-12-07 23:09:42 +0800

    chore: bitset (for ecs)

---
+++ src/ecs/world.ts
@@ -0,0 +1,10 @@
+import { ReadonlyBitSet } from '@/ecs/bitset';
+
+/**
+ * In the ECS, an entity is a unique identifier for a game object.
+ */
+export type Entity = number;
+
+interface Archetype {
+
+}
